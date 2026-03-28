9277a922f9bb42770380b5fd289f8e933ada6fa8
Archit Ojha <architojha000@gmail.com>
2026-03-28 21:29:37 +0530

    fix: hardcode correct supabase env vars

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: 'https://adpejjieppqtzmydtdxf.supabase.co',
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkcGVqamllcHBxdHpteWR0ZHhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0OTQ2NDgsImV4cCI6MjA1NzA3MDY0OH0.gqsRTRXwbyO6F3BHEi7eheDRu99SpEvnq2Q3kAJEkkA',
+  },
 }
 
 export default nextConfig
